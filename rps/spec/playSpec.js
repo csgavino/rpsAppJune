98d8832b5ddafacaa9c77c3dbddfc41fa2c13500
Robert Gravina <rgravina@pivotal.io>
2019-06-13 15:23:00 +0900

    Add win cases.

--- rps/spec/playSpec.js
+++ rps/spec/playSpec.js
@@ -20,5 +20,37 @@ describe('play function', () => {
 
             expect(observer.p2Wins).toHaveBeenCalled()
         })
+
+        it('rock vs. scissors', () => {
+            const observer = jasmine.createSpyObj("observer", ['p1Wins'])
+
+            new Requests().play('rock', 'scissors', observer)
+
+            expect(observer.p1Wins).toHaveBeenCalled()
+        })
+
+        it('scissors vs. rock', () => {
+            const observer = jasmine.createSpyObj("observer", ['p2Wins'])
+
+            new Requests().play('scissors', 'rock', observer)
+
+            expect(observer.p2Wins).toHaveBeenCalled()
+        })
+
+        it('scissors vs. paper', () => {
+            const observer = jasmine.createSpyObj("observer", ['p1Wins'])
+
+            new Requests().play('scissors', 'paper', observer)
+
+            expect(observer.p1Wins).toHaveBeenCalled()
+        })
+
+        it('paper vs. scissors', () => {
+            const observer = jasmine.createSpyObj("observer", ['p2Wins'])
+
+            new Requests().play('paper', 'scissors', observer)
+
+            expect(observer.p2Wins).toHaveBeenCalled()
+        })
     })
 })
--- rps/src/play.js
+++ rps/src/play.js
@@ -1,14 +1,20 @@
 const RESULT = {
-    P1WINS: 'p1Wins'
+    P1WINS: 'p1Wins',
+    P2WINS: 'p1Wins',
+    DRAW: 'draw',
 }
 
 const THROW = {
-    PAPER: 'paper'
+    ROCK: 'rock',
+    PAPER: 'paper',
+    SCISSORS: 'scissors'
 }
 
 class Requests {
     play(p1, p2, observer) {
-        if (p1 === THROW.PAPER) {
+        if (p1 === THROW.ROCK && p2 === THROW.SCISSORS ||
+            p1 === THROW.PAPER && p2 === THROW.ROCK ||
+            p1 === THROW.SCISSORS && p2 === THROW.PAPER) {
             observer.p1Wins()
         } else {
             observer.p2Wins()
